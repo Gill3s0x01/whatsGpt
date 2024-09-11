2a7e6debe79552ad2c419b224a2c5253b3036f91
Gill3s0x01 <54437398+Gill3s0x01@users.noreply.github.com>
2024-09-11 12:37:48 -0300

    Update route.ts

--- nextjs/src/app/api/generate-token/route.ts
+++ nextjs/src/app/api/generate-token/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: NextRequest) {
   const body = await request.json();
   const user = {
     name: "admin",
-    sub: body.user_id ?? "29958301-1e88-4994-bh1e-46h650924fh6",
+    sub: body.user_id ?? "token",
   };
 
   const secret = process.env.NEXTAUTH_SECRET as string;
